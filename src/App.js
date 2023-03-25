822ff8e4a327d55f17e71d8aed5f1f98c91e726b
Shirley Virginia Umpiérrez <119756319+sumpierrezf@users.noreply.github.com>
2023-03-25 17:46:17 +0000

    corrigiendo clases

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className='profile'>
         <div id='date'>Jueves 23 de marzo</div>
         <h1>Hola! <FaRegSmileBeam className='smile'/></h1>
-        <span>Vamos a cumplir esas metas!</span>
+        <span>Vamos a cumplir nuestras metas!</span>
       </div>
       <div className='add-task'>
         <input type='text' id='input' placeholder='Add a task'/>
@@ -18,7 +18,7 @@ function App() {
         <ul id='list'>
           <li>
             <FaRegCircle className='icons' data='done' id='0'/>
-            <p className='text line-through'>hola</p>
+            <p className='text'>hola</p>
             <FaTrashAlt className='icons' data='deleted' id='0'/>
           </li>
         </ul>
